2988323f6837c9328a28f4ccb8b97c35720cb79d
apierre <aurel.pierre.pro@gmail.com>
2026-08-07 14:13:01 +0200

    update flake install app

--- flake.nix
+++ flake.nix
@@ -9,21 +9,53 @@
   };
 
   outputs = { self, nixpkgs, disko, ... }:
+  let
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+
+    install = pkgs.writeShellApplication {
+      name = "install";
+
+      text = ''
+        set -euo pipefail
+
+        echo "Formatting and mounting disks..."
+
+        nix run ${disko} -- \
+          --mode destroy,format,mount \
+          ${./filesystems/disk.nix}
+
+        echo "Generating hardware configuration..."
+
+        nixos-generate-config --no-filesystems --root /mnt
+
+        echo "Creating local flake..."
+
+        cp ${./installer/flake.template.nix} /mnt/etc/nixos/flake.nix
+
+        echo "Installing..."
+
+        nixos-install --flake /mnt/etc/nixos#local
+      '';
+    };
+  in
   {
     nixosConfigurations.nixos =
       nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+        inherit system;
 
         modules = [
           disko.nixosModules.disko
-
-            ./default.nix
+          ./default.nix
         ];
       };
 
-    apps.x86_64-linux.install = {
+    apps.${system}.install = {
       type = "app";
-      program = "${self}/install.sh";
+      program = "${self}/bin/install";
     };
   };
 }
