50289d4fb2623428f116ee23fa7b0f92a61841bb
apierre <aurel.pierre.pro@gmail.com>
2026-08-07 13:34:31 +0200

    fix install.sh error

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-nix run github:nix-community/disko/latest -- --mode destroy,format,mount ./filesystems/disk.nix
+nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount ./filesystems/disk.nix
 nixos-generate-config --no-filesystems --root /mnt
 mv ./installer/flake.template.nix /mnt/etc/nixos/flake.nix
 nixos-install --flake /mnt/etc/nixos#local 
